7bd09b625617298c49a5131f1b061edd63f3d934
marcinkozakk <marcinkozakk@gmail.com>
2019-04-28 23:20:04 +0200

    User verification

--- database/migrations/2018_10_18_000000_create_users_table.php
+++ database/migrations/2018_10_18_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->string('name');
             $table->string('photo')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
             $table->rememberToken();
         });
