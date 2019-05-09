be23cb66f78759acb31cd2ab7a8031622e09b58f
marcinkozakk <marcinkozakk@gmail.com>
2019-05-09 16:44:09 +0200

    Docs

--- app/Http/Middleware/Language.php
+++ app/Http/Middleware/Language.php
@@ -5,10 +5,11 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
+use Request;
 
 class Language
 {
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         if (session()->has('locale') && array_key_exists(session()->get('locale'), Config::get('app.availableLanguages'))) {
             App::setLocale(session()->get('locale'));
